93d84c8c8afc4b8828d033f3a958a40335fecee4
jared <jaredgold@proton.me>
2023-08-14 17:29:54 -0400

    refactor to map at return, pass arrow func onclick, randomize Ids on start

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
-import { useState } from 'react'
-import './App.css'
-import { Card } from './components/card'
+import { useState } from "react";
+import "./App.css";
+import { Card } from "./components/card";
+import { randomCardOrder } from "./logic";
+
+const urlIds = randomCardOrder([
+  "YwAgyCddum3K0",
+  "QuxqWk7m9ffxyfoa0a",
+  "ighdr7xNaVwwQtUkP0",
+  "h8OX1iXpgYoH1wIkHE",
+  "5zI213WKIL2uI",
+  "3pQA6Uba4Argk",
+  "jpbAaUG7cjkZy",
+  "qTD9EXZRgI1y0",
+  "XcL6a7YHm4sWA",
+  "kBrY0BlY4C4jhBeubb",
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [urlId, setUrlIds] = useState(urlIds);
 
   return (
     <>
-      <div className="card-container">
-        <Card imgId={'YwAgyCddum3K0'} alt={''}></Card>
-        <Card imgId={'QuxqWk7m9ffxyfoa0a'} alt={''}></Card>
-        <Card imgId={'ighdr7xNaVwwQtUkP0'} alt={''}></Card>
-        <Card imgId={'h8OX1iXpgYoH1wIkHE'} alt={''}></Card>
-        <Card imgId={'5zI213WKIL2uI'} alt={''}></Card>
-        <Card imgId={'3pQA6Uba4Argk'} alt={''}></Card>
-        <Card imgId={'jpbAaUG7cjkZy'} alt={''}></Card>
-        <Card imgId={'qTD9EXZRgI1y0'} alt={''}></Card>
-        <Card imgId={'XcL6a7YHm4sWA'} alt={''}></Card>
-        <Card imgId={'kBrY0BlY4C4jhBeubb'} alt={''}></Card>
-      </div>     
+      {urlId.map((id) => (
+        <Card
+          imgId={id}
+          key={id}
+          fn={() => {
+            setUrlIds(randomCardOrder(urlId));
+          }}
+        ></Card>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
